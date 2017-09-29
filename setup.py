3913e43f5bfeaec86cb65981bb6af87fc822b85f
Tim Harder <radhermit@gmail.com>
2017-09-29 02:59:18 -0400

    drop py34 support

--- setup.py
+++ setup.py
@@ -24,7 +24,6 @@ setup(
     classifiers=(
         'Intended Audience :: Developers',
         'License :: OSI Approved :: BSD License',
-        'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
     ),
