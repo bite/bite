1214fb77975ec5993248cf6e142a0b5413ded948
Tim Harder <radhermit@gmail.com>
2017-10-25 00:48:31 -0400

    bite.const: rework XDG var determination

--- src/bite/const.py
+++ src/bite/const.py
@@ -6,6 +6,7 @@ from snakeoil import mappings
 from bite import __title__
 
 _reporoot = os.path.realpath(__file__).rsplit(os.path.sep, 3)[0]
+_module = sys.modules[__name__]
 try:
     # This is a file written during installation;
     # if it exists, we defer to it. If it doesn't, then we're
@@ -18,7 +19,7 @@ except ImportError:
 
 
 def _GET_CONST(attr, default_value):
-    consts = mappings.ProxiedAttrs(sys.modules[__name__])
+    consts = mappings.ProxiedAttrs(_module)
     is_tuple = not isinstance(default_value, str)
     if is_tuple:
         default_value = tuple(x % consts for x in default_value)
@@ -31,20 +32,13 @@ def _GET_CONST(attr, default_value):
     return result
 
 
-if 'XDG_CONFIG_HOME' in os.environ:
-    USER_CONFIG_PATH = os.path.join(os.environ['XDG_CONFIG_HOME'], __title__)
-else:
-    USER_CONFIG_PATH = os.path.expanduser(os.path.join('~/.config', __title__))
-
-if 'XDG_CACHE_HOME' in os.environ:
-    USER_CACHE_PATH = os.path.join(os.environ['XDG_CACHE_HOME'], __title__)
-else:
-    USER_CACHE_PATH = os.path.expanduser(os.path.join('~/.cache', __title__))
-
-if 'XDG_DATA_HOME' in os.environ:
-    USER_DATA_PATH = os.path.join(os.environ['XDG_DATA_HOME'], __title__)
-else:
-    USER_DATA_PATH = os.path.expanduser(os.path.join('~/.local/share', __title__))
+# determine XDG compatible paths
+for xdg_var, var_name, fallback_dir in (
+        ('XDG_CONFIG_HOME', 'USER_CONFIG_PATH', '~/.config'),
+        ('XDG_CACHE_HOME', 'USER_CACHE_PATH', '~/.cache'),
+        ('XDG_DATA_HOME', 'USER_DATA_PATH', '~/.local/share')):
+    setattr(_module, var_name,
+            os.environ.get(xdg_var, os.path.join(os.path.expanduser(fallback_dir), __title__)))
 
 DATA_PATH = _GET_CONST('DATA_PATH', _reporoot)
 if CONFIG_PATH is None:
