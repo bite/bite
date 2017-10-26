b12e4c0d232511f6c00c95b2f39d6136bb07f85b
Tim Harder <radhermit@gmail.com>
2017-10-26 18:11:03 -0400

    bite.services: split out generic xmlrpc support from bugzilla-xmlrpc into a base service
    So it can be easily used by other services providing XML-RPC interfaces.

---
+++ src/bite/services/_xmlrpc.py
@@ -0,0 +1,63 @@
+from xmlrpc.client import dumps, getparser, Fault
+
+from . import Service
+from ..exceptions import RequestError
+
+
+class Xmlrpc(Service):
+    """Support generic XML-RPC services."""
+
+    def __init__(self, **kw):
+        super().__init__(**kw)
+        self.session.headers.update({
+            'Accept': 'text/xml',
+            'Content-Type': 'text/xml'
+        })
+
+    def encode_request(self, method, params):
+        """Encode the data body for an XML-RPC request."""
+        encoding = 'utf-8'
+        return dumps((params,), method, encoding=encoding,
+                     allow_none=False).encode(encoding, 'xmlcharrefreplace')
+
+    def parse_response(self, response):
+        """Send request object and perform checks on the response."""
+        try:
+            return self._parse_xml(_IterContent(response))[0]
+        except Fault as e:
+            raise RequestError(msg=e.faultString, code=e.faultCode)
+
+    @staticmethod
+    def _parse_xml(response):
+        """Parse XML data from response."""
+        stream = response
+
+        p, u = getparser(use_datetime=True)
+
+        while 1:
+            data = stream.read(64*1024)
+            if not data:
+                break
+            try:
+                p.feed(data)
+            except Exception:
+                raise RequestError('error decoding response, XML-RPC interface likely disabled on server')
+
+        if stream is not response:
+            stream.close()
+        p.close()
+
+        return u.close()
+
+
+class _IterContent(object):
+
+    def __init__(self, file, size=64*1024):
+        self.initial = True
+        self.chunks = file.iter_content(chunk_size=size)
+
+    def read(self, size=64*1024):
+        try:
+            return next(self.chunks)
+        except StopIteration:
+            return
--- src/bite/services/bugzilla/xmlrpc.py
+++ src/bite/services/bugzilla/xmlrpc.py
@@ -1,35 +1,20 @@
-from xmlrpc.client import dumps, getparser, Fault
-
 from . import Bugzilla, BugzillaAttachment
+from .._xmlrpc import Xmlrpc
 from ...objects import decompress
 from ...exceptions import RequestError, AuthError
 
 
-class BugzillaXmlrpc(Bugzilla):
+class BugzillaXmlrpc(Bugzilla, Xmlrpc):
     """Support Bugzilla's deprecated XML-RPC interface."""
 
     def __init__(self, **kw):
         kw['endpoint'] = '/xmlrpc.cgi'
         super().__init__(**kw)
-        self.session.headers.update({
-            'Accept': 'text/xml',
-            'Content-Type': 'text/xml'
-        })
         self.attachment = BugzillaAttachmentXml
-
-    def encode_request(self, method, params):
-        """Encode the data body for an XML-RPC request."""
-        encoding = 'utf-8'
-        return dumps((params,), method, encoding=encoding,
-                     allow_none=False).encode(encoding, 'xmlcharrefreplace')
 
     def parse_response(self, response):
         """Send request object and perform checks on the response."""
-        try:
-            data = self._parse_xml(_IterContent(response))[0]
-        except Fault as e:
-            raise RequestError(msg=e.faultString, code=e.faultCode)
-
+        data = super().parse_response(response)
         if not data.get('faults', None):
             return data
         else:
@@ -38,40 +23,6 @@ class BugzillaXmlrpc(Bugzilla):
                 raise AuthError('access denied')
             raise RequestError(msg=error.get('faultString'), code=error.get('faultCode'))
 
-    @staticmethod
-    def _parse_xml(response):
-        """Parse XML data from response."""
-        stream = response
-
-        p, u = getparser(use_datetime=True)
-
-        while 1:
-            data = stream.read(64*1024)
-            if not data:
-                break
-            try:
-                p.feed(data)
-            except Exception:
-                raise RequestError('error decoding response, XML-RPC interface likely disabled on server')
-
-        if stream is not response:
-            stream.close()
-        p.close()
-
-        return u.close()
-
-
-class _IterContent(object):
-
-    def __init__(self, file, size=64*1024):
-        self.initial = True
-        self.chunks = file.iter_content(chunk_size=size)
-
-    def read(self, size=64*1024):
-        try:
-            return next(self.chunks)
-        except StopIteration:
-            return
 
 class BugzillaAttachmentXml(BugzillaAttachment):
 
