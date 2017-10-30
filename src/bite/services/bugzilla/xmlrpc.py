7f09e263d2bda6816b9eb2be5f0da569c22cc7b0
Tim Harder <radhermit@gmail.com>
2017-10-30 10:41:59 -0400

    services.bugzilla.xmlrpc: add more info about xml parsing failures
    It might be possible to use a different parser that can possibly handle
    or recover from bad input in a better fashion (e.g. lxml).

--- src/bite/services/bugzilla/xmlrpc.py
+++ src/bite/services/bugzilla/xmlrpc.py
@@ -17,6 +17,10 @@ class BugzillaXmlrpc(Bugzilla, Xmlrpc):
         try:
             data = super().parse_response(response)
         except ParsingError as e:
+            # The default expat parser has issues with certain data, e.g.
+            # https://bugs.gentoo.org/532044 -- running a get command against
+            # that bug returns an invalid token parsing error which isn't hit
+            # by the json parser.
             msg = e.msg + ", use the jsonrpc interface if available"
             raise ParsingError(msg=msg, text=e.text)
         except RequestError as e:
