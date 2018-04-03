f085389922154bd364119b1b149827cbc9b84c22
Tim Harder <radhermit@gmail.com>
2018-04-03 05:37:39 -0400

    client.bugzilla: default to None for nonexistent fields

--- src/bite/client/bugzilla/__init__.py
+++ src/bite/client/bugzilla/__init__.py
@@ -509,7 +509,7 @@ class Bugzilla(Cli):
                         yield value
         elif fields and output:
             for change in changes:
-                values = (getattr(change, field) for field in fields)
+                values = (getattr(change, field, None) for field in fields)
                 yield from self._iter_lines(output.format(*values))
         else:
             changes = list(str(x) for x in changes)
@@ -582,7 +582,7 @@ class Bugzilla(Cli):
                         yield value
         elif fields and output:
             for comment in comments:
-                values = (getattr(comment, field) for field in fields)
+                values = (getattr(comment, field, None) for field in fields)
                 yield from self._iter_lines(output.format(*values))
         else:
             comments = list(str(x) for x in comments)
