63abf094d9143f8f54f8d80800c0df124a6d0c3e
Tim Harder <radhermit@gmail.com>
2018-04-24 11:44:28 -0400

    service._jsonrest: explicitly handle 400 errors as well
    Since they generally contain an error message why the REST request was
    bad.

--- src/bite/service/_jsonrest.py
+++ src/bite/service/_jsonrest.py
@@ -21,6 +21,6 @@ class JsonREST(Json):
 
     def _failed_http_response(self, response):
         # catch invalid REST API resource requests
-        if response.status_code in (404,):
+        if response.status_code in (400, 404):
             self.parse_response(response)
         super()._failed_http_response(response)
