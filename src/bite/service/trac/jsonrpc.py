e64c14ea4e888fbb6bdaf0c14a1fa408eb5b3a0d
Tim Harder <radhermit@gmail.com>
2018-04-29 12:57:59 -0400

    service.trac.jsonrpc: decode datetime __jsonclass__ objects to datetimes

--- src/bite/service/trac/jsonrpc.py
+++ src/bite/service/trac/jsonrpc.py
@@ -1,13 +1,29 @@
 """Support Trac's JSON-RPC interface."""
 
+from dateutil.parser import parse as dateparse
+
 from . import Trac, GetItemRequest
 from .._jsonrpc import Jsonrpc, Multicall
 from .._reqs import req_cmd
+from ...utc import utc
+
+
+def as_datetime(dct):
+    """Decode datetime objects in data responses while decoding json."""
+    if '__jsonclass__' in dct:
+        type, val = dct['__jsonclass__']
+        if type == 'datetime':
+            # trac doesn't specify an offset for its timestamps, assume UTC
+            return dateparse(val).astimezone(utc)
+    return dct
 
 
 class TracJsonrpc(Trac, Jsonrpc):
 
     _service = 'trac-jsonrpc'
+
+    def parse_response(self, response):
+        return super().parse_response(response, object_hook=as_datetime)
 
 
 @req_cmd(TracJsonrpc)
