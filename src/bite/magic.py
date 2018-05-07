3c54e281c4ab85386b0348d8807dca16c3513e12
Tim Harder <radhermit@gmail.com>
2018-05-07 02:24:48 -0400

    update bundled magic

--- src/bite/magic.py
+++ src/bite/magic.py
@@ -41,14 +41,18 @@ Usage:
 
 import sys
 import glob
-import os.path
 import ctypes
 import ctypes.util
 import threading
 
 from ctypes import c_char_p, c_int, c_size_t, c_void_p
 
-class MagicException(Exception): pass
+
+class MagicException(Exception):
+    def __init__(self, message):
+        super(MagicException, self).__init__(message)
+        self.message = message
+
 
 class Magic:
     """
@@ -70,7 +74,7 @@ class Magic:
         self.flags = MAGIC_NONE
         if mime:
             self.flags |= MAGIC_MIME
-        elif mime_encoding:
+        if mime_encoding:
             self.flags |= MAGIC_MIME_ENCODING
         if keep_going:
             self.flags |= MAGIC_CONTINUE
@@ -79,33 +83,34 @@ class Magic:
             self.flags |= MAGIC_COMPRESS
 
         self.cookie = magic_open(self.flags)
+        self.lock = threading.Lock()
 
         magic_load(self.cookie, magic_file)
-
-        self.thread = threading.currentThread()
 
     def from_buffer(self, buf):
         """
         Identify the contents of `buf`
         """
-        self._thread_check()
-        try:
-            return magic_buffer(self.cookie, buf).decode()
-        except MagicException as e:
-            return self._handle509Bug(e)
+        with self.lock:
+            try:
+                # if we're on python3, convert buf to bytes
+                # otherwise this string is passed as wchar*
+                # which is not what libmagic expects
+                if type(buf) == str and str != bytes:
+                    buf = buf.encode('utf-8', errors='replace')
+                return maybe_decode(magic_buffer(self.cookie, buf))
+            except MagicException as e:
+                return self._handle509Bug(e)
 
     def from_file(self, filename):
-        """
-        Identify the contents of file `filename`
-        raises IOError if the file does not exist
-        """
-        self._thread_check()
-        if not os.path.exists(filename):
-            raise IOError("File does not exist: " + filename)
-        try:
-            return magic_file(self.cookie, filename)
-        except MagicException as e:
-            return self._handle509Bug(e)
+        # raise FileNotFoundException or IOError if the file does not exist
+        with open(filename):
+            pass
+        with self.lock:
+            try:
+                return maybe_decode(magic_file(self.cookie, filename))
+            except MagicException as e:
+                return self._handle509Bug(e)
 
     def _handle509Bug(self, e):
         # libmagic 5.09 has a bug where it might fail to identify the
@@ -113,13 +118,8 @@ class Magic:
         # likely _buffer), but also does not return an error message.
         if e.message is None and (self.flags & MAGIC_MIME):
             return "application/octet-stream"
-
-    def _thread_check(self):
-        if self.thread != threading.currentThread():
-            raise Exception('attempting to use libmagic on multiple threads will '
-                            'end in SEGV.  Prefer to use the module functions '
-                            'from_file or from_buffer, or carefully manage direct '
-                            'use of the Magic class')
+        else:
+            raise e
 
     def __del__(self):
         # no _thread_check here because there can be no other
@@ -136,14 +136,15 @@ class Magic:
             magic_close(self.cookie)
             self.cookie = None
 
+_instances = {}
 
-instances = threading.local()
 
 def _get_magic_type(mime):
-    i = instances.__dict__.get(mime)
+    i = _instances.get(mime)
     if i is None:
-        i = instances.__dict__[mime] = Magic(mime=mime)
+        i = _instances[mime] = Magic(mime=mime)
     return i
+
 
 def from_file(filename, mime=False):
     """"
@@ -156,6 +157,7 @@ def from_file(filename, mime=False):
     """
     m = _get_magic_type(mime)
     return m.from_file(filename)
+
 
 def from_buffer(buffer, mime=False):
     """
@@ -170,25 +172,28 @@ def from_buffer(buffer, mime=False):
     return m.from_buffer(buffer)
 
 
-
-
 libmagic = None
 # Let's try to find magic or magic1
-dll = ctypes.util.find_library('magic') or ctypes.util.find_library('magic1') or ctypes.util.find_library('cygmagic-1')
+dll = ctypes.util.find_library('magic') \
+    or ctypes.util.find_library('magic1') \
+    or ctypes.util.find_library('cygmagic-1')
 
-# This is necessary because find_library returns None if it doesn't find the library
+# necessary because find_library returns None if it doesn't find the library
 if dll:
     libmagic = ctypes.CDLL(dll)
 
 if not libmagic or not libmagic._name:
-    windows_dlls = ['magic1.dll','cygmagic-1.dll']
+    windows_dlls = ['magic1.dll', 'cygmagic-1.dll']
     platform_to_lib = {'darwin': ['/opt/local/lib/libmagic.dylib',
                                   '/usr/local/lib/libmagic.dylib'] +
-                         # Assumes there will only be one version installed
-                         glob.glob('/usr/local/Cellar/libmagic/*/lib/libmagic.dylib'),
+                       # Assumes there will only be one version installed
+                       glob.glob('/usr/local/Cellar/libmagic/*/lib/libmagic.dylib'),  # flake8:noqa
                        'win32': windows_dlls,
-                       'cygwin': windows_dlls }
-    for dll in platform_to_lib.get(sys.platform, []):
+                       'cygwin': windows_dlls,
+                       'linux': ['libmagic.so.1'],  # fallback for some Linuxes (e.g. Alpine) where library search does not work # flake8:noqa
+                      }
+    platform = 'linux' if sys.platform.startswith('linux') else sys.platform
+    for dll in platform_to_lib.get(platform, []):
         try:
             libmagic = ctypes.CDLL(dll)
             break
@@ -216,16 +221,28 @@ def errorcheck_negative_one(result, func, args):
         return result
 
 
+# return str on python3.  Don't want to unconditionally
+# decode because that results in unicode on python2
+def maybe_decode(s):
+    if str == bytes:
+        return s
+    else:
+        return s.decode('utf-8')
+
 def coerce_filename(filename):
     if filename is None:
         return None
 
     # ctypes will implicitly convert unicode strings to bytes with
-    # .encode('ascii').  If you use the filesystem encoding 
+    # .encode('ascii').  If you use the filesystem encoding
     # then you'll get inconsistent behavior (crashes) depending on the user's
     # LANG environment variable
-    if isinstance(filename, str):
-        return filename.encode('utf-8')
+    is_unicode = (sys.version_info[0] <= 2 and
+                  isinstance(filename, unicode)) or \
+                  (sys.version_info[0] >= 3 and
+                   isinstance(filename, str))
+    if is_unicode:
+        return filename.encode('utf-8', 'surrogateescape')
     else:
         return filename
 
@@ -285,43 +302,24 @@ magic_compile.argtypes = [magic_t, c_char_p]
 
 
 MAGIC_NONE = 0x000000 # No flags
-
 MAGIC_DEBUG = 0x000001 # Turn on debugging
-
 MAGIC_SYMLINK = 0x000002 # Follow symlinks
-
 MAGIC_COMPRESS = 0x000004 # Check inside compressed files
-
 MAGIC_DEVICES = 0x000008 # Look at the contents of devices
-
 MAGIC_MIME = 0x000010 # Return a mime string
-
 MAGIC_MIME_ENCODING = 0x000400 # Return the MIME encoding
-
 MAGIC_CONTINUE = 0x000020 # Return all matches
-
 MAGIC_CHECK = 0x000040 # Print warnings to stderr
-
 MAGIC_PRESERVE_ATIME = 0x000080 # Restore access time on exit
-
 MAGIC_RAW = 0x000100 # Don't translate unprintable chars
-
 MAGIC_ERROR = 0x000200 # Handle ENOENT etc as real errors
 
 MAGIC_NO_CHECK_COMPRESS = 0x001000 # Don't check for compressed files
-
 MAGIC_NO_CHECK_TAR = 0x002000 # Don't check for tar files
-
 MAGIC_NO_CHECK_SOFT = 0x004000 # Don't check magic entries
-
 MAGIC_NO_CHECK_APPTYPE = 0x008000 # Don't check application type
-
 MAGIC_NO_CHECK_ELF = 0x010000 # Don't check for elf details
-
 MAGIC_NO_CHECK_ASCII = 0x020000 # Don't check for ascii files
-
 MAGIC_NO_CHECK_TROFF = 0x040000 # Don't check ascii/troff
-
 MAGIC_NO_CHECK_FORTRAN = 0x080000 # Don't check ascii/fortran
-
 MAGIC_NO_CHECK_TOKENS = 0x100000 # Don't check ascii/tokens
