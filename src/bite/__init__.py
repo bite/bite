d339f81c2ea48dc3f724eb8bfe01b677f9b7731b
Tim Harder <radhermit@gmail.com>
2018-05-22 22:59:42 -0400

    bite: add get_service() method to return a service obj for a configured service.

--- src/bite/__init__.py
+++ src/bite/__init__.py
@@ -1,9 +1,14 @@
-__title__ = 'bite'
-__version__ = '0.0.1'
-
 from importlib import import_module
 
+from snakeoil.demandload import demandload
+
+from .config import load_service_files
 from .exceptions import BiteError
+
+demandload('bite:const')
+
+__title__ = 'bite'
+__version__ = '0.0.1'
 
 
 def get_service_cls(service, options):
@@ -18,3 +23,14 @@ def get_service_cls(service, options):
     return getattr(import_module(mod_name), cls_name)
 
 
+def get_service(connection):
+    """Return a service object for a configured service."""
+    # support getting passed service objects and service name strings
+    config = load_service_files(connection)
+
+    if not config.has_section(connection):
+        raise BiteError(f'unknown connection: {connection!r}')
+
+    args = dict(config.items(connection))
+    service_obj = get_service_cls(args['service'], const.SERVICES)(**args)
+    return service_obj
