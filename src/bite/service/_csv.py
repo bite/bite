3525a488f811e0c85302403f0a1cd40e326bb691
Tim Harder <radhermit@gmail.com>
2018-06-07 13:07:36 -0400

    service._csv: verify content type in response headers

--- src/bite/service/_csv.py
+++ src/bite/service/_csv.py
@@ -2,6 +2,7 @@ import csv
 import io
 
 from ._reqs import URLRequest
+from ..exceptions import RequestError
 
 
 class CSVRequest(URLRequest):
@@ -9,6 +10,13 @@ class CSVRequest(URLRequest):
 
     def parse_response(self, response):
         """Parse the raw CSV content."""
+        if not response.headers.get('Content-Type', '').startswith('text/csv'):
+            msg = 'non-CSV response from server'
+            if not self.service.verbose:
+                msg += ' (use verbose mode to see it)'
+            raise RequestError(
+                msg, code=response.status_code, text=response.text, response=response)
+
         # Requesting the text content of the response doesn't remove the BOM so
         # we request the binary content and decode it ourselves to remove it.
         f = io.StringIO(response.content.decode('utf-8-sig'))
