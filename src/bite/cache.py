111f5c1409f1d11a7ecde86020553739e98b2e4a
Tim Harder <radhermit@gmail.com>
2018-07-02 22:17:43 -0400

    cache: Auth: initial support for encrypting/decrypting auth tokens

--- src/bite/cache.py
+++ src/bite/cache.py
@@ -140,13 +140,14 @@ class Cache(object):
 
 class Auth(object):
 
-    def __init__(self, connection, path=None, token=None):
+    def __init__(self, connection, path=None, token=None, gpgkeys=()):
         self.token = token
+        self._gpgkeys = gpgkeys
 
         if path is not None:
             self.path = path
         elif connection is not None:
-            self.path = os.path.join(const.USER_CACHE_PATH, 'auth', connection)
+            self.path = os.path.join(const.USER_CACHE_PATH, 'auth', f'{connection}.gpg')
         else:
             self.path = None
 
@@ -157,22 +158,43 @@ class Auth(object):
         if token is None:
             return
 
-        if self.path is not None:
+        # don't overwrite custom auth files
+        if self.path is not None and self.path.endswith('.gpg'):
+            try:
+                with gpg.Context() as c:
+                    cipertext, _result, _sign_result = c.encrypt(
+                        token.encode(), recipients=self._gpgkeys, sign=False)
+            except gpg.errors.GpgError as e:
+                raise BiteError(f'failed encrypting auth token: {e}')
+
             os.makedirs(os.path.dirname(self.path), exist_ok=True)
             try:
-                with open(self.path, 'w+') as f:
-                    os.chmod(self.path, stat.S_IREAD | stat.S_IWRITE)
-                    f.write(token)
+                with open(self.path, 'wb') as f:
+                    f.write(cipertext)
+                os.chmod(self.path, stat.S_IREAD | stat.S_IWRITE)
             except (PermissionError, IsADirectoryError) as e:
-                raise BiteError(f'failed writing auth token to {self.path!r}: {e.strerror}')
+                raise BiteError(f'failed writing auth token: {self.path!r}: {e.strerror}')
 
     def read(self):
-        if self.path is not None:
-            try:
-                with open(self.path, 'r') as f:
-                    self.token = f.read().strip()
-            except IOError:
-                self.token = None
+        if self.path is not None and os.path.exists(self.path):
+            if self.path.endswith('.gpg'):
+                try:
+                    with open(self.path, 'rb') as f:
+                        try:
+                            with gpg.Context() as c:
+                                plaintext, _result, _verify_result = c.decrypt(f)
+                        except gpg.errors.GpgError as e:
+                            raise BiteError(f'failed decrypting auth token: {self.path!r}: {e}')
+                    token = plaintext.decode().strip()
+                except IOError as e:
+                    raise BiteError(f'failed reading auth token: {self.path!r}: {e}')
+            else:
+                try:
+                    with open(self.path, 'r') as f:
+                        token = f.read().strip()
+                except IOError as e:
+                    raise BiteError(f'failed reading auth token: {self.path!r}: {e}')
+            self.token = token
 
     def update(self, token):
         self.token = token
