8ca20c7aed1bb22871a48d5d59e391503a1f711a
Tim Harder <radhermit@gmail.com>
2020-11-02 02:23:10 -0700

    setup.py: bump supported python versions

--- setup.py
+++ setup.py
@@ -119,6 +119,8 @@ setup(
         'License :: OSI Approved :: BSD License',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
     ],
     **pkgdist_setup
 )
