287654ca1ba05d5e8b36c90206906e0b4311c827
Tim Harder <radhermit@gmail.com>
2021-01-01 12:04:24 -0700

    drop py36/py37 support

--- setup.py
+++ setup.py
@@ -117,8 +117,6 @@ setup(
     classifiers=[
         'Intended Audience :: Developers',
         'License :: OSI Approved :: BSD License',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
     ],
